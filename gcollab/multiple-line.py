957d103dc9da9db271fa73c478062c3e045dbae9
Abdu-Azzam <abdu22073si@student.nurulfikri.ac.id>
2025-11-26 13:06:21 +0700

    Praktikum3

---
+++ gcollab/multiple-line.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt 
+
+#Buat data sample
+months = ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+product_A_sales = [10,20,15,25,30,45,40,50,60,55,65,70]
+product_B_sales = [5,10,8,15,18,20,22,30,25,35,40,45]
+
+#Multiple
+plt.plot(months, product_A_sales, label='Product A', marker='o') 
+plt.plot(months, product_B_sales, label='Product B', marker='x')
+plt.title('Penjualan Produk Per Bulan')
+plt.xlabel('Bulan')
+plt.ylabel('Jumlah Penjualan')
+plt.legend()
+plt.show
